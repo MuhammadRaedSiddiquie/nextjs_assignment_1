c7a65e2baeb43b1808bec4f375e85de3ddec8f52
MuhammadRaedSiddiquie <bhairaed636@gmail.com>
2024-10-27 12:32:30 +0500

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div  className="flex items-center justify-center h-screen w-full bg-green-200 flex-col gap-6">
+      <h2 className="text-white text-4xl">About Us:</h2>
+      <p className='w-[70%] text-lg text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quod ipsa, placeat earum fugiat officia veniam, similique soluta distinctio maxime unde accusantium possimus cupiditate architecto rem nam adipisci aliquid. A animi molestias, deleniti asperiores cumque dicta incidunt molestiae ipsa odit reiciendis cupiditate, sit eum magnam beatae fugiat ducimus quibusdam magni, quasi nesciunt earum? Nihil, nam delectus, corrupti asperiores nemo tenetur architecto tempora et, est maxime doloremque nisi corporis reiciendis rem harum error facilis! Hic magni natus dicta delectus debitis, temporibus eius architecto eos itaque optio quisquam dolor corporis culpa quos libero nesciunt facilis animi odio cupiditate praesentium quibusdam consectetur qui? Eveniet dolorum nostrum asperiores, repellat nulla error facilis voluptatem accusantium corporis temporibus sunt porro mollitia explicabo! Eligendi, voluptates perferendis. Dicta natus cum est, placeat, iure reprehenderit harum nesciunt porro ratione sequi repellendus laboriosam vel molestias sunt iusto aliquid amet? Voluptates suscipit sit modi quo provident, obcaecati labore. Placeat, ullam cupiditate. Fugiat rerum obcaecati, consequatur ex repellat adipisci ducimus tempore accusantium omnis corporis vel dignissimos debitis optio ea modi iure nostrum suscipit voluptate ipsam nesciunt tenetur. Sunt quaerat voluptate velit, nihil fugiat doloribus perferendis placeat aliquam error obcaecati. Totam soluta perferendis error! Reprehenderit dicta nihil maiores non blanditiis atque quas voluptates.</p>
+    </div>
+  )
+}
+
+export default About
